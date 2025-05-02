aece93ec55005de204c07069d83ba02bbacec932
carlos warley <carloswarley0@gmail.com>
2025-05-02 04:59:39 -0300

    schedulingStructure

---
+++ client/src/app/features/employee/components/EmployeeCard.tsx
@@ -0,0 +1,7 @@
+export default function EmployeeCard () {
+    return(
+        <div className="flex flex-col items-center justify-center gap-5 min-h-screen flex-1 sm:mx-0 mx-5">
+            <h1></h1>
+        </div>
+    )
+}
